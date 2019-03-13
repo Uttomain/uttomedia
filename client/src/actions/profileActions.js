23fa5d2c6ee04e54b2f9427a6e7d45b78fb6e19d
Usman Kiyani <uttomain@gmail.com>
2019-03-13 13:28:03 +0100

    Profile component done

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -20,6 +20,24 @@ export const getCurrentProfile = () => (dispatch) => {
     );
 }
 
+// Get Profile By Handle
+export const getProfileByHandle = (handle) => (dispatch) => {
+  dispatch(setProfileLoading());
+  axios.get(`/api/profile/handle/${handle}`)
+    .then(res => 
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data
+      })  
+    )
+    .catch(err => 
+      dispatch({
+        type: GET_PROFILE,
+        payload: null
+      })
+    );
+}
+
 // Get All Profiles
 export const getProfiles = () => (dispatch) => {
   dispatch(setProfileLoading());
